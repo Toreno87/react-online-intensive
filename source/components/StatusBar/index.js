9c8be5f12bb957a196f1f1a1d82dee5dd0187eb2
Konstantin <tocet87@gmail.com>
2019-03-22 12:28:00 +0200

    rafactor code

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -3,17 +3,20 @@ import React, { Component } from 'react';
 
 // Instruments
 import Styles from './styles.m.css';
-import avatar from 'theme/assets/lisa';
 
 export default class StatusBar extends Component {
     render() {
+        const {
+            avatar,
+            currentUserFirstName,
+            currentUserLastName,
+        } = this.props;
+
         return (
             <section className = { Styles.statusBar } >
                 <button>
                     <img src = { avatar } />
-                    <span>Lisa</span>
-                    &nbsp;
-                    <span>Simpson</span>
+                    <span>{`${currentUserFirstName} ${currentUserLastName}`}</span>
                 </button>
             </section>
         );

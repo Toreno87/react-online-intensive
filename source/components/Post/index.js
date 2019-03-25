99d024e48872b1cba7161ee61e57eb8aae43e4cb
Konstantin <tocet87@gmail.com>
2019-03-25 12:00:52 +0200

    add context to Composer and Post component

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -1,25 +1,26 @@
 // Core
 import React, { Component } from 'react';
 
+//Components
+import { Consumer } from 'components/HOC/withProfile';
+
 // Instruments
 import Styles from './styles.m.css';
 import moment from 'moment';
 
 export default class Post extends Component {
     render() {
-        const {
-            avatar,
-            currentUserFirstName,
-            currentUserLastName,
-        } = this.props;
-
         return (
-            <section className = { Styles.post }>
-                <img src = { avatar } />
-                <a>{`${currentUserFirstName} ${currentUserLastName}`}</a>
-                <time>{ moment().format('MMMM D h:mm:ss a') }</time>
-                <p>Hwday</p>
-            </section>
+            <Consumer>
+                {(context) => (
+                    <section className = { Styles.post }>
+                        <img src = { context.avatar } />
+                        <a>{`${context.currentUserFirstName} ${context.currentUserLastName}`}</a>
+                        <time>{ moment().format('MMMM D h:mm:ss a') }</time>
+                        <p>Hwday</p>
+                    </section>
+                )}
+            </Consumer>
         );
     }
 }

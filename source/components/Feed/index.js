2fa116d280b783bbd0371841d25f8dad24da487b
Konstantin <tocet87@gmail.com>
2019-04-02 13:02:30 +0300

    add likes

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -10,40 +10,99 @@ import Spinner from 'components/Spinner';
 
 // Instrumnets
 import Styles from './styles.m.css';
-import { getUniqueID } from 'instruments';
+import { getUniqueID, delay } from 'instruments';
 
 export default class Feed extends Component {
     constructor () {
         super();
 
         this._createPost = this._createPost.bind(this);
+        this._setPostFetchingState = this._setPostFetchingState.bind(this);
+        this._likePost = this._likePost.bind(this);
     }
 
     state = {
         posts: [
-            { id: '123', comment: 'Hi, I am here!', created: 1526825076849 },
-            { id: '456', comment: 'Привет!', created: 1526825076555 },
+            {
+                id: '123',
+                comment: 'Hi, I am here!',
+                created: 1526825076849,
+                likes: [],
+            },
+            {
+                id: '456',
+                comment: 'Привет!',
+                created: 1526825076555,
+                likes: [],
+            },
         ],
-        isPostsFetching: true,
+        isPostsFetching: false,
     };
 
-    _createPost (comment) {
+    _setPostFetchingState (state) {
+        this.setState({
+            isPostsFetching: state,
+        });
+    }
+
+    async _createPost (comment) {
+        this._setPostFetchingState(true);
+
         const post = {
             id: getUniqueID(),
             created: moment.utc(),
             comment: comment,
+            likes: [],
         };
+
+        await delay(1200);
 
         this.setState(({ posts }) => ({
             posts: [ post, ...posts ],
+            isPostsFetching: false,
         }));
+    }
+
+    async _likePost(id) {
+        const { currentUserFirstName, currentUserLastName} = this.props;
+        this._setPostFetchingState(true);
+
+        await delay(1200);
+
+        const newPosts = this.state.posts.map((post) => {
+            if (post.id === id) {
+                return {
+                    ...post,
+                    likes: [
+                        {
+                            id: getUniqueID(),
+                            firstName: currentUserFirstName,
+                            lastName: currentUserLastName,
+                        },
+                    ],
+                };
+            }
+
+            return post;
+        });
+
+        this.setState({
+            posts: newPosts,
+            isPostsFetching: false,
+        });
     }
 
     render() {
         const { posts, isPostsFetching } = this.state;
 
         const postJSX = posts.map((post) => {
-            return <Post key = { post.id } { ...post } />;
+            return (
+                <Post
+                    key = { post.id }
+                    { ...post }
+                    _likePost = { this._likePost }
+                />
+            );
         });
 
         return (
@@ -56,3 +115,4 @@ export default class Feed extends Component {
         );
     }
 }
+

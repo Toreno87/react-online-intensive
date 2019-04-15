007b5c40b223d99211633e4f048dc1e96a625910
Konstantin <tocet87@gmail.com>
2019-04-15 10:00:08 +0300

    add Postman component

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,5 +1,7 @@
 // Core
 import React, { Component } from 'react';
+import { Transition } from 'react-transition-group';
+import { fromTo } from 'gsap';
 
 // Components
 import { withProfile } from 'components/HOC/withProfile';
@@ -8,6 +10,7 @@ import Composer from 'components/Composer';
 import Post from 'components/Post';
 import StatusBar from 'components/StatusBar';
 import Spinner from 'components/Spinner';
+import Postman from 'components/Postman';
 
 
 // Instrumnets
@@ -137,6 +140,10 @@ export default class Feed extends Component {
         }));
     }
 
+    _animateComposerEnter = (composer) => {
+        fromTo(composer, 1, { opacity: 0, rotationX: 50 }, { opacity: 1, rotationX: 0 });
+    }
+
     render() {
         const { posts, isPostsFetching } = this.state;
 
@@ -156,7 +163,14 @@ export default class Feed extends Component {
             <section className = { Styles.feed }>
                 < Spinner isSpinning = { isPostsFetching } />
                 < StatusBar />
-                < Composer _createPost = { this._createPost } />
+                <Transition
+                    appear
+                    in
+                    timeout = { 4000 }
+                    onEnter = { this._animateComposerEnter }>
+                    < Composer _createPost = { this._createPost } />
+                </Transition>
+                <Postman />
                 { postJSX }
             </section>
         );
